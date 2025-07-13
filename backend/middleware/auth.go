d4c4f7dd0643344552fbf2f3a6d964bbbfa1dd6f
h0lm0 <h0lm0@users.noreply.github.com>
2025-07-13 15:18:14 +0200

    casbin begin

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/casbin/casbin/v2"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +19,36 @@ func AuthRequired() gin.HandlerFunc {
 			return
 		}
 		c.Set("user_id", userID)
+		c.Next()
+	}
+}
+
+func CheckPolicy(obj string, act string, enforcer *casbin.Enforcer) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		sub, existed := c.Get("user_id")
+		if !existed {
+			c.AbortWithStatusJSON(401, gin.H{"msg": "User hasn't logged in yet"})
+			return
+		}
+
+		err := enforcer.LoadPolicy()
+		if err != nil {
+			c.AbortWithStatusJSON(500, gin.H{"msg": "Failed to load policy from DB"})
+			return
+		}
+
+		// Casbin enforces policy
+		ok, err := enforcer.Enforce(fmt.Sprint(sub), obj, act)
+
+		if err != nil {
+			c.AbortWithStatusJSON(500, gin.H{"msg": "Error occurred when authorizing user"})
+			return
+		}
+
+		if !ok {
+			c.AbortWithStatusJSON(403, gin.H{"msg": "You are not authorized"})
+			return
+		}
 		c.Next()
 	}
 }
