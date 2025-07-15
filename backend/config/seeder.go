4024d95399976ed9079f4d32cce2d98dc098b5f7
h0lm0 <h0lm0@users.noreply.github.com>
2025-07-15 22:17:02 +0200

    Casbin seeder update (load csv), database seeder update, front register fix

--- backend/config/seeder.go
+++ backend/config/seeder.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"pwnthemall/models"
 
+	"github.com/casbin/casbin/v2"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
@@ -27,14 +28,13 @@ func seedChallengeCategory() {
 			log.Printf("Failed to seed challengeCategory %s: %v\n", challengeCategory.Name, err)
 		}
 	}
-	log.Println("challengeCategories seeded")
+	log.Println("Seeding: challengeCategories finished")
 }
 
 func seedDefaultUsers() {
 	users := []models.User{
-		{Username: "swinowz", Email: "mail@swinowz.com", Password: "swinowz"},
-		{Username: "h0lm", Email: "mail@h0lm.com", Password: "h0lm"},
-		{Username: "credentials", Email: "mail@credentials.com", Password: "credentials"},
+		{Username: "admin", Email: "admin@admin.admin", Password: "admin", Role: "admin"},
+		{Username: "user", Email: "user@user.user", Password: "user", Role: "member"},
 	}
 
 	for _, user := range users {
@@ -58,10 +58,47 @@ func seedDefaultUsers() {
 			log.Printf("Failed to seed user %s: %v\n", user.Username, err)
 		}
 	}
-	log.Println("users seeded")
+	log.Println("Seeding: users finished")
+}
+
+func SeedCasbin(enforcer *casbin.Enforcer) {
+	log.Println("Seeding: Casbin rules..")
+	if hasPolicy, _ := enforcer.HasPolicy("anonymous", "/login", "*"); !hasPolicy {
+		enforcer.AddPolicy("anonymous", "/login", "*")
+	}
+	if hasPolicy, _ := enforcer.HasPolicy("anonymous", "/register", "*"); !hasPolicy {
+		enforcer.AddPolicy("anonymous", "/register", "*")
+	}
+
+	if hasPolicy, _ := enforcer.HasPolicy("member", "/pwn", "*"); !hasPolicy {
+		enforcer.AddPolicy("member", "/pwn", "*")
+	}
+	if hasPolicy, _ := enforcer.HasPolicy("member", "/logout", "*"); !hasPolicy {
+		enforcer.AddPolicy("member", "/logout", "*")
+	}
+
+	if hasPolicy, _ := enforcer.HasPolicy("admin", "/*", "*"); !hasPolicy {
+		enforcer.AddPolicy("admin", "/*", "*")
+	}
+	enforcer.SavePolicy()
+	log.Println("Seeding: Casbin finished")
+
+}
+
+func SeedCasbinFromCsv(enforcer *casbin.Enforcer) {
+	log.Println("Seeding: Casbin rules from CSV..")
+	e, err := casbin.NewEnforcer("casbin_model.conf", "casbin_policies.csv")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	e.LoadPolicy()
+	e.SetAdapter(enforcer.GetAdapter())
+	e.SavePolicy()
+	log.Println("Seeding: Casbin from CSV finished")
 }
 
 func SeedDatabase() {
+	log.Println("Seeding: Database..")
 	seedChallengeCategory()
 	seedDefaultUsers()
 }
