4165892a35b1f0ffe398c0617c61e3a6ae24cb64
h0lm0 <h0lm0@users.noreply.github.com>
2025-07-15 22:17:02 +0200

    Casbin seeder update (load csv), database seeder update, front register fix

--- backend/config/casbin.go
+++ backend/config/casbin.go
@@ -10,32 +10,13 @@ import (
 
 var CEF *casbin.Enforcer
 
-func seedCasbin() {
-	if hasPolicy, _ := CEF.HasPolicy("admin", "/*", "*"); !hasPolicy {
-		CEF.AddPolicy("admin", "/*", "*")
-	}
-	if hasPolicy, _ := CEF.HasPolicy("anonymous", "/login", "*"); !hasPolicy {
-		CEF.AddPolicy("anonymous", "/login", "*")
-	}
-	if hasPolicy, _ := CEF.HasPolicy("anonymous", "/register", "*"); !hasPolicy {
-		CEF.AddPolicy("anonymous", "/register", "*")
-	}
-	if hasPolicy, _ := CEF.HasPolicy("member", "/pwn", "*"); !hasPolicy {
-		CEF.AddPolicy("member", "/pwn", "*")
-	}
-	if hasPolicy, _ := CEF.HasPolicy("member", "/logout", "*"); !hasPolicy {
-		CEF.AddPolicy("member", "/logout", "*")
-	}
-	CEF.SavePolicy()
-}
-
 func InitCasbin() *casbin.Enforcer {
 	adapter, err := gormadapter.NewAdapterByDB(DB)
 	if err != nil {
 		panic(fmt.Sprintf("failed to initialize casbin adapter: %v", err))
 	}
 
-	enforcer, err := casbin.NewEnforcer("rbac_model.conf", adapter)
+	enforcer, err := casbin.NewEnforcer("casbin_model.conf", adapter)
 	if err != nil {
 		panic(fmt.Sprintf("failed to create casbin enforcer: %v", err))
 	}
@@ -43,7 +24,11 @@ func InitCasbin() *casbin.Enforcer {
 	CEF = enforcer
 
 	if os.Getenv("SEED_CASBIN") == "true" {
-		seedCasbin()
+		SeedCasbin(CEF)
+	}
+
+	if os.Getenv("SEED_CASBIN_CSV") == "true" {
+		SeedCasbinFromCsv(CEF)
 	}
 
 	return enforcer
