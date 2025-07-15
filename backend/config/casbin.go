401e7a3d03779eb8ef47290d66ac6be754625d9a
h0lm0 <h0lm0@users.noreply.github.com>
2025-07-15 19:29:48 +0200

    casbin policies ok

--- backend/config/casbin.go
+++ backend/config/casbin.go
@@ -2,12 +2,32 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
 )
 
 var CEF *casbin.Enforcer
+
+func seedCasbin() {
+	if hasPolicy, _ := CEF.HasPolicy("admin", "/*", "*"); !hasPolicy {
+		CEF.AddPolicy("admin", "/*", "*")
+	}
+	if hasPolicy, _ := CEF.HasPolicy("anonymous", "/login", "*"); !hasPolicy {
+		CEF.AddPolicy("anonymous", "/login", "*")
+	}
+	if hasPolicy, _ := CEF.HasPolicy("anonymous", "/register", "*"); !hasPolicy {
+		CEF.AddPolicy("anonymous", "/register", "*")
+	}
+	if hasPolicy, _ := CEF.HasPolicy("member", "/pwn", "*"); !hasPolicy {
+		CEF.AddPolicy("member", "/pwn", "*")
+	}
+	if hasPolicy, _ := CEF.HasPolicy("member", "/logout", "*"); !hasPolicy {
+		CEF.AddPolicy("member", "/logout", "*")
+	}
+	CEF.SavePolicy()
+}
 
 func InitCasbin() *casbin.Enforcer {
 	adapter, err := gormadapter.NewAdapterByDB(DB)
@@ -20,13 +40,11 @@ func InitCasbin() *casbin.Enforcer {
 		panic(fmt.Sprintf("failed to create casbin enforcer: %v", err))
 	}
 	enforcer.LoadPolicy()
-	if hasPolicy, _ := enforcer.HasPolicy("admin", "user", "write"); !hasPolicy {
-		enforcer.AddPolicy("admin", "user", "write")
-	}
-	if hasPolicy, _ := enforcer.HasPolicy("user", "challenge", "read"); !hasPolicy {
-		enforcer.AddPolicy("user", "challenge", "read")
-	}
-	enforcer.SavePolicy()
 	CEF = enforcer
+
+	if os.Getenv("SEED_CASBIN") == "true" {
+		seedCasbin()
+	}
+
 	return enforcer
 }
--- backend/routes/user.go
+++ backend/routes/user.go
@@ -10,10 +10,10 @@ import (
 func RegisterUserRoutes(router *gin.Engine) {
 	users := router.Group("/users", middleware.AuthRequired())
 	{
-		users.GET("", middleware.CheckPolicy("user", "read"), controllers.GetUsers)
-		users.GET("/:id", middleware.CheckPolicy("user", "read"), controllers.GetUser)
-		users.POST("", middleware.CheckPolicy("user", "write"), controllers.CreateUser)
-		users.PUT("/:id", middleware.CheckPolicy("user", "write"), controllers.UpdateUser)
-		users.DELETE("/:id", middleware.CheckPolicy("user", "write"), controllers.DeleteUser)
+		users.GET("", middleware.CheckPolicy("member", "read"), controllers.GetUsers)
+		users.GET("/:id", middleware.CheckPolicy("admin", "read"), controllers.GetUser)
+		users.POST("", middleware.CheckPolicy("admin", "write"), controllers.CreateUser)
+		users.PUT("/:id", middleware.CheckPolicy("admin", "write"), controllers.UpdateUser)
+		users.DELETE("/:id", middleware.CheckPolicy("admin", "write"), controllers.DeleteUser)
 	}
 }
