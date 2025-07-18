b03228c68f1097afe2321ad4016b845b1ef26220
h0lm0 <h0lm0@users.noreply.github.com>
2025-07-18 09:07:28 +0200

    Challenges implementation (front + back) + front refacto component

--- backend/routes/challenge.go
+++ backend/routes/challenge.go
@@ -12,6 +12,8 @@ func RegisterChallengeRoutes(router *gin.Engine) {
 	{
 		challenges.GET("", middleware.CheckPolicy("/challenges", "read"), controllers.GetChallenges)
 		challenges.GET("/:id", middleware.CheckPolicy("/challenges/:id", "read"), controllers.GetChallenge)
+		challenges.GET("/category/:category", middleware.CheckPolicy("/challenges/category/:category", "read"), controllers.GetChallengesByCategoryName)
+
 		challenges.POST("", middleware.CheckPolicy("/challenges", "write"), controllers.CreateChallenge)
 		// challenges.PUT("/:id", middleware.CheckPolicy("/challenges/:id", "write"), controllers.UpdateUser)
 		// challenges.DELETE("/:id", middleware.CheckPolicy("/challenges/:id", "write"), controllers.DeleteUser)
