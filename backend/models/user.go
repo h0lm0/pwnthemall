4ce92a4fde9e8cf4b7f43c9cbeb8a4eaf00ff5f1
swinowz <swinowz@swinowz.swinowz>
2025-07-20 20:00:37 +0200

    push

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	ID        uint        `gorm:"primaryKey" json:"id"`
-	Username  string      `gorm:"unique;not null" json:"username"`
-	Email     string      `gorm:"unique;not null" json:"email"`
+	Username  string      `gorm:"unique;not null;size:32" json:"username"`
+	Email     string      `gorm:"unique;not null;size:254" json:"email"`
 	Password  string      `json:"-"`
 	Role      string      `gorm:"not null;default:'member'" json:"role"`
 	CreatedAt time.Time   `json:"createdAt"`
