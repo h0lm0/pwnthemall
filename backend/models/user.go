20fe13a2f490e2faa3fae08b704fb98c4caa4fb1
h0lm0 <h0lm0@users.noreply.github.com>
2025-07-20 19:27:40 +0200

    New backend models

--- backend/models/user.go
+++ backend/models/user.go
@@ -5,14 +5,14 @@ import (
 )
 
 type User struct {
-	ID        uint        `gorm:"primaryKey" json:"id"`
-	Username  string      `gorm:"unique;not null" json:"username"`
-	Email     string      `gorm:"unique;not null" json:"email"`
-	Password  string      `json:"-"`
-	Role      string      `gorm:"not null;default:'member'" json:"role"`
-	CreatedAt time.Time   `json:"createdAt"`
-	UpdatedAt time.Time   `json:"updatedAt"`
-	TeamID    *uint       `json:"teamId,omitempty"`
-	Team      *Team       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"team,omitempty"`
-	Solves    []Challenge `gorm:"many2many:solves" json:"solves,omitempty"`
+	ID          uint         `gorm:"primaryKey" json:"id"`
+	Username    string       `gorm:"unique;not null" json:"username"`
+	Email       string       `gorm:"unique;not null" json:"email"`
+	Password    string       `json:"-"`
+	Role        string       `gorm:"not null;default:'member'" json:"role"`
+	CreatedAt   time.Time    `json:"createdAt"`
+	UpdatedAt   time.Time    `json:"updatedAt"`
+	TeamID      *uint        `json:"teamId,omitempty"`
+	Team        *Team        `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"team,omitempty"`
+	Submissions []Submission `gorm:"many2many:submissions" json:"submissions,omitempty"`
 }
