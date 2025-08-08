3792bd5df77762b75852bc44b2be749eda166a61
h0lm0 <h0lm0@users.noreply.github.com>
2025-08-08 23:22:47 +0200

    demo env

--- backend/routes/instance.go
+++ backend/routes/instance.go
@@ -8,7 +8,7 @@ import (
 )
 
 func RegisterInstanceRoutes(router *gin.Engine) {
-	challenges := router.Group("/instances")
+	challenges := router.Group("/instances", middleware.DemoRestriction)
 	{
 		challenges.GET("", middleware.AuthRequiredTeamOrAdmin(), middleware.CheckPolicy("/instances", "read"), controllers.GetInstances)
 		challenges.GET("/:id", middleware.AuthRequiredTeamOrAdmin(), middleware.CheckPolicy("/instances/:id", "read"), controllers.GetInstance)
