69101af73c6bb3c9cb3b96818fab0f393b212047
h0lm0 <h0lm0@users.noreply.github.com>
2025-11-04 19:27:43 +0100

    fix/dto.UserInput role property (incorrect oneof)

--- backend/dto/user.go
+++ backend/dto/user.go
@@ -5,6 +5,6 @@ type UserInput struct {
 	Username string `json:"username" binding:"required,max=32"`
 	Email    string `json:"email" binding:"required,email,max=254"`
 	Password string `json:"password" binding:"omitempty,min=8,max=72"`
-	Role     string `json:"role" binding:"required,oneof=user admin"`
+	Role     string `json:"role" binding:"required,oneof=member admin"`
 	TeamID   *uint  `json:"teamId"`
 }
